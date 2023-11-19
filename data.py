ee04627fbd23bb9d297c63bc3ecfd58f84e5c9e2
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-11-19 04:20:05 +0000

    New data/train

---
+++ data.py
@@ -0,0 +1,81 @@
+import itertools
+import logging
+
+import numpy as np
+from datasets import load_dataset
+from transformers import CLIPTokenizerFast
+from transformers.utils.logging import set_verbosity_warning
+
+from trainable_clip_model import TrainableCLIPModel
+from wavelet_spectrogram import cwt_spectrogram
+
+set_verbosity_warning()
+logging.basicConfig(filename='train.log', level=logging.INFO)
+
+dataset = load_dataset("DavidVivancos/MindBigData2022_MNIST_IN")
+logging.info(list(dataset["train"][0].items())[:8])
+
+# %%
+channels = sorted(set(map(lambda x: x.split("-")[0], dataset["train"][0].keys())) - {'label'})
+logging.info(channels)
+
+
+# %%
+# B, C, H, W
+C = len(channels)
+H, W = 168, 256
+L = 256
+S = max(H, W)
+
+
+# %%
+map_digit_to_token = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+}
+
+
+# %%
+tokenizer = CLIPTokenizerFast.from_pretrained("openai/clip-vit-large-patch14")
+
+map_digit_to_input_ids = {
+    digit: tokenizer(f"{map_digit_to_token[digit]}", padding=True).input_ids
+    for digit in range(10)
+}
+
+def preprocess(inputs):
+  B = len(inputs["label"])
+  inputs["input_ids"] = [map_digit_to_input_ids[label] for label in inputs["label"]]
+  inputs["attention_mask"] = [[1] * 3] * B
+  samples = np.zeros((B, C, S, S))
+  for b in range(B):
+    for i, channel in enumerate(channels):
+      sample = np.array([inputs[f"{channel}-{j}"][b] for j in range(L)])
+      power, *_ = cwt_spectrogram(sample, 120, nNotes=24, detrend=True, normalize=True)
+      samples[b, i, :min(H, S), :min(W, S)] = power.squeeze()
+  inputs["pixel_values"] = samples
+  return inputs
+
+
+# %%
+item = preprocess(dataset["train"][:8])
+item["label"], item["input_ids"], item['pixel_values'].shape
+
+
+# %%
+remove_columns = [f"{channel}-{i}" for channel, i in itertools.product(channels, range(L))] + ["label"]
+
+
+# %%
+preprocessed_dataset = dataset.map(preprocess, batched=True, remove_columns=remove_columns)
+
+# %%
+preprocessed_dataset.save_to_disk("eeg_mnist_preprocessed")
--- train.py
+++ train.py
@@ -1,9 +1,6 @@
-import itertools
 import logging
 
-import numpy as np
 from datasets import load_dataset
-from torch.utils.data import DataLoader
 from transformers import (
     CLIPProcessor,
     CLIPTokenizerFast,
@@ -12,13 +9,9 @@ from transformers import (
     Trainer,
     TrainingArguments,
 )
-from transformers.utils.logging import set_verbosity_warning
-
+from torch import nn
 from trainable_clip_model import TrainableCLIPModel
-from wavelet_spectrogram import cwt_spectrogram
-
-set_verbosity_warning()
-logging.basicConfig(filename='train.log', level=logging.INFO)
+logging.basicConfig(filename='train.log', stream=sys.stdout, level=logging.INFO)
 
 dataset = load_dataset("DavidVivancos/MindBigData2022_MNIST_IN")
 logging.info(list(dataset["train"][0].items())[:8])
@@ -37,38 +30,20 @@ S = max(H, W)
 
 
 # %%
-map_digit_to_token = {
-    0: "zero",
-    1: "one",
-    2: "two",
-    3: "three",
-    4: "four",
-    5: "five",
-    6: "six",
-    7: "seven",
-    8: "eight",
-    9: "nine",
-}
-
-
-# %%
 tokenizer = CLIPTokenizerFast.from_pretrained("openai/clip-vit-large-patch14")
 
-
 # %%
-preprocessed_dataset = load_dataset("eeg_mnist_preprocessed")
-
-# %%
+hidden_size = 64
 configuration = CLIPVisionConfig(
     image_size=max(H, W),
     num_channels=C,
     num_hidden_layers=1,
     # hidden_size (`int`, *optional*, defaults to 768):
     #     Dimensionality of the encoder layers and the pooler layer.
-    hidden_size=128,
+    hidden_size=hidden_size,
     # intermediate_size (`int`, *optional*, defaults to 3072):
     #     Dimensionality of the "intermediate" (i.e., feed-forward) layer in the Transformer encoder.
-    intermediate_size=4*128,
+    intermediate_size=4*hidden_size,
     # projection_dim (`int`, *optional*, defaults to 512):
     #     Dimentionality of text and vision projection layers.
     # num_hidden_layers (`int`, *optional*, defaults to 12):
@@ -87,7 +62,7 @@ processor = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
 
 # %%
 model.vision_model = vision_model.vision_model
-
+model.visual_projection = nn.Linear(hidden_size, model.projection_dim, bias=False)
 
 # %%
 for p in model.text_model.parameters():
@@ -102,6 +77,10 @@ logging.info(f"All Parameters: {sum(p.numel() for p in model.parameters()) / 1e6
 logging.info(f"Trainable Parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad) / 1e6}M")
 
 
+
+# %%
+preprocessed_dataset = load_dataset("eeg_mnist_preprocessed", keep_in_memory=True)
+
 # %%
 preprocessed_dataset['train'], preprocessed_dataset['validation'] = preprocessed_dataset['train'].train_test_split(.1).values()
 
@@ -109,15 +88,12 @@ preprocessed_dataset['train'], preprocessed_dataset['validation'] = preprocessed
 # %%
 training_args = TrainingArguments(
     output_dir="clep_trainer",
-    per_device_train_batch_size=256,
-    per_device_eval_batch_size=256,
+    per_device_train_batch_size=64,
+    per_device_eval_batch_size=64,
     logging_steps=1,
     tf32=True,
     num_train_epochs=5,
-    lr_scheduler_type="cosine",
-    learning_rate=5e-4,
-    weight_decay=0.1,
-    warmup_steps=1_000,
+    learning_rate=1e-3,
     evaluation_strategy="epoch",
     save_strategy="epoch",
     save_total_limit=3,
