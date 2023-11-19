230a8b848c23a2c32ca88dde1c2012e83d703323
Tony <ruotongs@outlook.com>
2023-11-18 20:09:02 -0700

    lstm tweak2

--- lstm_model.py
+++ lstm_model.py
@@ -4,14 +4,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class EEGFeatNet(nn.Module):
-    def __init__(self, n_channels, n_features=128, projection_dim=128, num_layers=1):
+    def __init__(self, n_channels, n_features, projection_dim, num_layers=1):
         super(EEGFeatNet, self).__init__()
         self.hidden_size = n_features
         self.num_layers = num_layers
 
         self.encoder = nn.LSTM(input_size=n_channels, hidden_size=self.hidden_size, num_layers=self.num_layers, batch_first=True)
         self.fc = nn.Linear(in_features=self.hidden_size, out_features=projection_dim, bias=False)
-
 
     def forward(
         self,
@@ -21,13 +20,14 @@ class EEGFeatNet(nn.Module):
         return_dict: Optional[bool] = None,
     ):
 
-        device = next(self.parameters()).device
+        device = self.parameters()
         x = pixel_values
         h_n = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
         c_n = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
 
         _, (h_n, c_n) = self.encoder(x, (h_n, c_n))
         x = h_n[-1]
+        x = self.fc(x)
 
         x = F.normalize(x, dim=-1)
 
